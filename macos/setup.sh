891f7b9ae72c70557f5eab2ca0ba20633eeba2dd
Guilherme Rodrigues <gadr90@gmail.com>
2018-12-10 14:27:05 -0200

    Remove sudo protection

--- macos/setup.sh
+++ macos/setup.sh
@@ -14,9 +14,6 @@ info "Setting macOS defaults..."
 # Ask for the administrator password upfront
 sudo -v
 
-# # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 # Set computer name
 sudo scutil --set ComputerName "$COMPUTER_NAME"
 sudo scutil --set LocalHostName "$COMPUTER_NAME"
