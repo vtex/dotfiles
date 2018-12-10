91567f03023db48c40f726f6f73cd7f66986d82e
Guilherme Rodrigues <gadr90@gmail.com>
2018-12-10 14:37:23 -0200

    Some fixes

--- spectacle/setup.sh
+++ spectacle/setup.sh
@@ -1,4 +1,7 @@
 #! /usr/bin/env bash
 
+DIR=$(dirname "$0")
+cd "$DIR"
+
 mkdir -p $HOME/Library/Application\ Support/Spectacle/
 cp ./Shortcuts.json $HOME/Library/Application\ Support/Spectacle/Shortcuts.json
