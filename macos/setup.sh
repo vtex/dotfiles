3e33f8513226d020e7f0c27b824b4419a740e35f
Guilherme Rodrigues <gadr90@gmail.com>
2018-12-10 14:27:26 -0200

    Fix typo

--- macos/setup.sh
+++ macos/setup.sh
@@ -371,4 +371,4 @@ defaults write com.google.Chrome.canary DisablePrintPreview -bool true
 defaults write com.google.Chrome PMPrintingExpandedStateForPrint2 -bool true
 defaults write com.google.Chrome.canary PMPrintingExpandedStateForPrint2 -bool true
 
-success "FInished setting macOS defaults. Note that some of these changes require a logout/restart to take effect."
+success "Finished setting macOS defaults. Note that some of these changes require a logout/restart to take effect."
